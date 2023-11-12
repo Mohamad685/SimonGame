0eb6a72add052614bf5b74a53b4858d06345f5d5
Mohamad685 <Mohammad.fakih685@gmail.com>
2023-11-12 22:05:22 +0200

    gameOver and restart game done with some errors

--- Simon Says/Script/script.js
+++ Simon Says/Script/script.js
@@ -2,40 +2,54 @@ const red= document.querySelector('#red')
 const blue= document.querySelector('#blue')
 const yellow= document.querySelector('#yellow')
 const green= document.querySelector('#green')
+
 const colors=["red","green","yellow","blue"]
+
 const redSound= document.querySelector('#sound1')
 const blueSound= document.querySelector('#sound3')
 const greenSound= document.querySelector('#sound2')
 const yellowSound= document.querySelector('#sound4')
+const headChange= document.getElementById("level-title")
+
 const colorSound = {
     red : redSound,
     blue: blueSound,
     yellow: yellowSound,
     green:greenSound,
 }
+
 const autoSequence=[]
 const userSequence=[]
+
 let level=0
 let gameBegin= false
-const headChange= document.getElementById("level-title")
-
 
 // //Functions section
 function userInputChecking(userlevel){
-        if (userSequence[userSequence.length - 1] === autoSequence[userSequence.length - 1]) {
-            if (userSequence.length === autoSequence.length) {
-                console.log("User has completed their sequence")
-                setTimeout(function() {
-                    autoSequencing()
-                }, 1000)
-                userSequence.length = 0 
-            }
-        }
+    if (userSequence[userSequence.length - 1] === autoSequence[userSequence.length - 1]) {
+        if (userSequence.length === autoSequence.length) {
+            console.log("User has completed their sequence")
+            setTimeout(function() {
+            autoSequencing()
+            }, 1000)
+            userSequence.length = 0        
     }
+    }else{ 
+        gameOver()
+        restartGame()
+    }   
+}
 
-
-
-
+function gameOver(){
+    const overSound =document.querySelector('#sound5')
+    overSound.play()
+    document.getElementsByTagName('body')[0].classList.add('game-over')
+    document.querySelector('h1').textContent ="Game Over, Press Any Key to Restart"
+    setTimeout(function(){
+        document.getElementsByTagName('body')[0].classList.remove('game-over')
+    }, 300)
+    document.querySelector('h1').textContent ="Game Over, Press Any Key to Restart"
+}
 
 // function to make an auto random sequence of colors
 function autoSequencing(){
@@ -82,7 +96,13 @@ function animatePress(color) {
             button.classList.remove('pressed')}, 100)
 }
 
+function restartGame(){
+    autoSequence.length=0
+    userSequence.length=0
+    level=0
+    gameBegin= false
 
+}
 
 document.addEventListener("click",function(e){
     if (!gameBegin){
